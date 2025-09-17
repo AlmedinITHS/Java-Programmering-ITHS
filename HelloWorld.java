569faf8523ba3d5a1e72e9897d87854c2df920e5
AlmedinITHS <Almedin.Beharovic@ITHS.se>
2025-09-17 11:15:52 +0200

    Whileloop added

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,7 +4,7 @@ public class HelloWorld {
     public static void main(String[] args) {
         int i = 1;
 
-        while (i < 0) {
+        while (i < 10) {
             System.out.println(i);
             i++;
         }
