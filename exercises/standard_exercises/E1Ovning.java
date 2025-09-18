838dec837955409acaa7aae372959f46dc5879c1
AlmedinITHS <Almedin.Beharovic@ITHS.se>
2025-09-18 14:05:04 +0200

    Added int

---
+++ exercises/standard_exercises/E1Ovning.java
@@ -0,0 +1,60 @@
+public class E1Ovning { //Namnet på filen
+    public static void main (String [] args) {
+        //1.1
+        String a;
+        int myAge;
+        myAge = 36;
+
+        int myAge = 36;
+        //1.2
+
+        //1.3
+        String myName = "Tom";
+        //1.4
+        boolean theTruth = false;
+        System.out.println(theTruth);
+        //1.5
+        int x = 256;
+        byte y = 8;
+        System.out.println(x);
+        System.out.println(y);
+
+        y = x;
+        System.out.println(x);
+        //1.6
+        int numberOfStudents = 23; //Ja
+        char = classroomIdentifier = 'B';//Ja
+        int backgroundColor = "blue"; // Nej
+        boolean above50%; // Nej
+        boolean isNative = 0; // Nej
+        String exampleString = "This is an example string"; //Ja
+        int gravitationalForce = 9.82; //Nej
+        double gravitationalForce = 9.82; //Ja
+        byte screenResolutionX = 1024; //Nej
+        char smileyFace = '\u263a'; //Unicode, Ja
+
+        //1.7
+
+        //1.10
+        int a = 10, b = 20, c = 30;
+        String firstName = "hello", lastName = "goodbye";
+        int average = (a + b + c)/3;
+
+        System.out.println(format: " The average is " + average);
+
+        int nummer = 56;
+         
+
+
+
+
+
+
+
+        
+
+
+
+
+    }
+}
