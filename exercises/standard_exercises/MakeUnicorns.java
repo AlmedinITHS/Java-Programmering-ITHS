32fba3085145b9acc5bc20116e2ba7e7c6f3d5ab
AlmedinITHS <Almedin.Beharovic@ITHS.se>
2025-09-19 10:47:00 +0200

    Two unicorns created

---
+++ exercises/standard_exercises/MakeUnicorns.java
@@ -0,0 +1,7 @@
+public class MakeUnicorns {
+    public static void main(String[] args) {
+        Unicorn bengt = new Unicorn(5, "Blå", "Glad", true, true)
+        Unicorn ilse = new Unicorn(4, "gul", "Likgiltig", false, false);
+    }
+    
+}
