2a2e7f61a3469d94dc575b16f75c23d27bed9cf7
AlmedinITHS <Almedin.Beharovic@ITHS.se>
2025-09-19 10:52:12 +0200

    Added two functioning unicorns.

--- exercises/standard_exercises/MakeUnicorns.java
+++ exercises/standard_exercises/MakeUnicorns.java
@@ -1,7 +1,7 @@
 public class MakeUnicorns {
     public static void main(String[] args) {
-        Unicorn bengt = new Unicorn(5, "Blå", "Glad", true, true)
-        Unicorn ilse = new Unicorn(4, "gul", "Likgiltig", false, false);
+        Unicorn Bengt = new Unicorn(5, "Blå", "Glad", true, true);
+        Unicorn Ilse = new Unicorn(4, "gul", "Likgiltig", false, false);
     }
     
 }
