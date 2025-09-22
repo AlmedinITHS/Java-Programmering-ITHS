b00ba9022e518a9736044ebe511d58a89453baa2
AlmedinITHS <Almedin.Beharovic@ITHS.se>
2025-09-22 15:10:50 +0200

    Övningar på slides från lektion 25/09/22

--- exercises/standard_exercises/Unicorn_program/Methods/distanceConverter.java
+++ exercises/standard_exercises/Unicorn_program/Methods/distanceConverter.java
@@ -1,4 +1,5 @@
 package Unicorn_program.Methods;
+import java.util.Scanner;// Behövs för att ta emot input med scanner
 
 public class distanceConverter {
     public static void main(String[] args) {
@@ -6,6 +7,22 @@ public class distanceConverter {
         milesToKilometers(4.7);
         milesToKilometers(6.75);
         milesToKilometers2();
+
+        System.out.println("Please input your age:");
+        Scanner scan = new Scanner(System.in); //Vi skapar ett nytt objekt från scannerklassen, döper den till scan
+        byte age = scan.nextByte();
+
+        System.out.println("Please input your name: ");
+        String name = scan.next();
+
+        System.out.println("Please input your initials, if any; ");
+        char nameInitials = scan.ne();
+
+        scan.close();
+
+        Scanner scan2 = new Scanner(System.in);
+        double distance = scan2.nextDouble();
+        System.out.println(distance);
     }
     static void milesToKilometers2() {
         double miles = 100;
@@ -17,6 +34,10 @@ public class distanceConverter {
         double kilometers = miles * 1.60934;
 
         System.out.println(kilometers);
+    }
+    static void kilometersToMiles(double kilometers) {
+        double miles = kilometers / 1.60934;
+        System.out.println(miles);
     }
     
 }
