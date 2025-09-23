e940fc1dd9d3b5ee0917cb2caf207b2d51689b22
AlmedinITHS <Almedin.Beharovic@ITHS.se>
2025-09-23 14:42:36 +0200

    Övnin 2.1 extra färdig

--- Övningar/OvningarLecture2ControlFlow.java
+++ Övningar/OvningarLecture2ControlFlow.java
@@ -28,12 +28,71 @@ public class OvningarLecture2ControlFlow {
         }
 
         int sum = 0;
-        for(int i = 0; i < 11; i++) {
+        for(int i = 0; i <= 10; i++) {
+            
+            if(i % 2 != 0) {
             sum = sum+ i;
-            if(i % 2 == 0)
-            continue;
             System.out.println(sum);
         }
+     } // Vf får jag värden i dubbletter?
+        
+        //2.1 extra
+        int a = 5, b = 10;
+        if ( a > b) {
+            b = a * b;
+        } 
+        else {
+            a = a + b;
+        }
+        System.out.println("a: " + a + " b: " + b);
+
+        int p = 15, q = 10;
+        if (p >= q) {
+            q = p - q;
+        }
+        else {
+            p = q - p;
+
+        }
+        System.out.println("p: " + p + " q: " + q);
+        
+        int m = 0, n = 6;
+        if (m == n) {
+            n = n * 2;
+        }
+        else {
+           m = m * n;
+        }
+        System.out.println(m);
+
+        int i = 4, j = 12;
+        if(i != j) {
+            j = j / i;
+        }
+        else {
+            i = i % j;
+        }
+        System.out.println(j);
+
+        int c = 7, d = 2;
+        if (c < d) {
+            d = c % d;
+        }
+        else {
+            c = c / d;
+        }
+        System.out.println("c: " + c + " d: " + d);
+
+        int e = 25, f = 30;
+        if (e <= f) {
+            f = f - e;
+        }
+        else {
+            e = e + f;
+        }
+        System.out.println("e: " + e + " f: " + f);
+
+
 
 
 
