a3c55959b8f7a56d1e373beae4a996d9336abe65
AlmedinITHS <Almedin.Beharovic@ITHS.se>
2025-09-23 11:07:21 +0200

    Forloop

--- Övningar/OvningarLecture2ControlFlow.java
+++ Övningar/OvningarLecture2ControlFlow.java
@@ -1,36 +1,12 @@
 public class OvningarLecture2ControlFlow {
     public static void main(String[] args) {
-        //a
-        int x = 3, y = 8;
-        if (x < y) {
-            y = x + y;
-        }
-        else {
-            y = y - x;
-        }
-        System.out.println(y);
+        
+
+        int result = 0;
+        for (int = 1; i < 5; i++)
+        result = result + i;
 
 
-        //b
-        x = 12; y = 200;
-        if (x == y) {
-            y = 80;
-        }
-        else {
-            y = x;
-        }
-        System.out.println(y);
-
-        //c
-        x = 12; y = 0;
-        y = x * x - 2 * x + 30;
-        if(x < y) {
-            y = 1;
-        }
-        else {
-            y = 0;
-        }
-        System.out.println(y);
     }
 
 }
