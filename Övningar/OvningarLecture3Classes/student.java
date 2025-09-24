3e7750f0872cd8f5c664e3e7b63bc3f82c17e956
AlmedinITHS <Almedin.Beharovic@ITHS.se>
2025-09-24 14:09:12 +0200

    Created a student with values

---
+++ Övningar/OvningarLecture3Classes/student.java
@@ -0,0 +1,25 @@
+package OvningarLecture3Classes;
+
+public class student {
+    private String name;
+    private int age;
+    private double grade;
+
+private student(String name, int age, double grade) {
+    this.name = name;
+    this.age = age;
+    this.grade = grade;
+}
+public void printDetails() {
+    System.out.println("Name: " + name);
+    System.out.println("Age: " + age);
+    System.out.println("Grade: " + grade);
+}
+public static void main(String[] args) {
+    student student = new student("Max", 30,7.7);
+    student.printDetails();
+}
+}
+
+
+
