f37d4497d2d206444b14f89da4dd7f32510a7a19
AlmedinITHS <Almedin.Beharovic@ITHS.se>
2025-09-25 12:19:39 +0200

    Instansiering av två objekt

---
+++ Övningar/OvningarLecture3Classes/Flygplats.java
@@ -0,0 +1,8 @@
+package OvningarLecture3Classes;
+
+public class Flygplats {
+    String name = "Landvetter flygplats";
+
+    Flygplan Embraer = new Flygplan();
+    Flygplan Boeing = new Flygplan();
+}
