869567a18c2b6df541810d03c9116064fdd55aea
AlmedinITHS <Almedin.Beharovic@ITHS.se>
2025-09-25 16:51:28 +0200

    Metoder av Beräkning för rektangel och cirkel färdiga

---
+++ Övningar/OvningarLecture4Metoder/Circle.java
@@ -0,0 +1,20 @@
+package OvningarLecture4Metoder;
+
+public class Circle {
+    private double radius;
+
+    public double cumputeArea() {
+        double area = radius * radius * Math.PI;
+        return area;
+    }
+    public double computeCircumference() {
+        double circumference = radius * 2 * Math.PI;
+        return circumference;
+    }
+    public double getRadius() {
+        return radius;
+    }
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+}
---
+++ Övningar/OvningarLecture4Metoder/Rectangle.java
@@ -0,0 +1,27 @@
+package OvningarLecture4Metoder;
+
+public class Rectangle {
+    private double a;
+    private double b;
+
+    public double computeArea() {
+        double area = a * b;
+        return area;
+    }
+    public double computePerimeter() {
+        double perimeter = a * 2 + b * 2;
+        return perimeter;
+    }
+    public double getA() {
+        return a;
+    }
+    public double getB() {
+        return b;
+    }
+    public void setA(double a) {
+        this.a = a;
+    }
+    public void setB(double b) {
+        this.b = b;
+    }
+}
