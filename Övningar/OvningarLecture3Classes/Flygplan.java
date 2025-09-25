3dca7a492a4ad00b2c7c55fe14579c9434d4bfad
AlmedinITHS <Almedin.Beharovic@ITHS.se>
2025-09-25 12:20:59 +0200

    Fields attribut tillagt för klassen

---
+++ Övningar/OvningarLecture3Classes/Flygplan.java
@@ -0,0 +1,7 @@
+package OvningarLecture3Classes;
+
+public class Flygplan {
+    public int maxPassagerare = 48;
+    public double NosTillBakvinge = 28.7;
+    public String modell = "Embraer ERJ 145";
+}
