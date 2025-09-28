f9a1a1ba90a34d3af9484dbfb33e769252a294e9
AlmedinITHS <Almedin.Beharovic@ITHS.se>
2025-09-28 22:36:55 +0200

    Enum planeter

---
+++ Övningar/OvningarLecture5Arrayer/Planeter.java
@@ -0,0 +1,14 @@
+package OvningarLecture5Arrayer;
+
+public enum Planeter {
+    Merkurius,
+    Venus,
+    Jorden,
+    Mars,
+    Jupiter,
+    Saturnus,
+    Uranus,
+    Neptunus,
+    Pluto;
+    
+}
