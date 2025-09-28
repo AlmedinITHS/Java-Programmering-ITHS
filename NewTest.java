8772c8223faf73e4aeffa62268665db048e3ac09
AlmedinITHS <Almedin.Beharovic@ITHS.se>
2025-09-28 21:17:03 +0200

    Lite övningar från pdfen

--- NewTest.java
+++ NewTest.java
@@ -1,7 +1,14 @@
 public class NewTest {
     public static void main(String[] args) {
-        
-        
+        int [] myArray = {3,5,10, 20,43,17};
+
+        int element = 0;
+
+        while (element < myArray.length) {
+            System.out.println(myArray[element]);
+            element++;
+        }
     }
-    
 }
+    
+
