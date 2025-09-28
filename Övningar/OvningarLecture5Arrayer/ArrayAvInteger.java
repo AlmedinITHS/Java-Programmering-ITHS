7f4f846017abb2fe921923354df2baed941b3f9f
AlmedinITHS <Almedin.Beharovic@ITHS.se>
2025-09-28 22:09:39 +0200

    Kopierat övningen....fattar ingenting av outputten i terminalen

---
+++ Övningar/OvningarLecture5Arrayer/ArrayAvInteger.java
@@ -0,0 +1,37 @@
+package OvningarLecture5Arrayer;
+
+import java.util.Arrays;
+import java.util.Random;
+
+public class ArrayAvInteger {
+    int [] integerArray;
+
+    public ArrayAvInteger(int i) {
+        integerArray = new int[i];
+        Random random = new Random();
+
+        for (int element = 0; element < integerArray.length; element++) {
+            integerArray[element] = random.nextInt(1000);
+        }
+    }
+    public static void main(String[] args) {
+        ArrayAvInteger myArray = new ArrayAvInteger(10);
+
+        System.out.println(Arrays.toString(myArray.integerArray));
+
+        myArray.substitute(4,999);
+
+        System.out.println(Arrays.toString(myArray.integerArray));
+
+        myArray.square(3);
+
+        System.out.println(Arrays.toString(myArray.integerArray));
+    }
+    void substitute (int index, int newValue) {
+        integerArray[index] = newValue;
+    }
+
+    void square(int index) {
+        integerArray[index] = integerArray[index] * integerArray[index];
+    }
+}
