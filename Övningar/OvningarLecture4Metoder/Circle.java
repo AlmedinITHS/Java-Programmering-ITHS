59e7d48d0baf92d7ee510321604bbdfdbb65e4ad
AlmedinITHS <Almedin.Beharovic@ITHS.se>
2025-09-30 14:38:06 +0200

    Repetition på övningen

--- Övningar/OvningarLecture4Metoder/Circle.java
+++ Övningar/OvningarLecture4Metoder/Circle.java
@@ -3,17 +3,18 @@ package OvningarLecture4Metoder;
 public class Circle {
     private double radius;
 
-    public double cumputeArea() {
+    public double CalculateArea() {
         double area = radius * radius * Math.PI;
         return area;
     }
-    public double computeCircumference() {
+    public double CalculateCircumference() {
         double circumference = radius * 2 * Math.PI;
         return circumference;
     }
     public double getRadius() {
         return radius;
     }
+
     public void setRadius(double radius) {
         this.radius = radius;
     }
