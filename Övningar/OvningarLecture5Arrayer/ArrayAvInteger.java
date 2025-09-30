18bc58539f5329d9b2aa0a674c40ff15f5b7dfab
AlmedinITHS <Almedin.Beharovic@ITHS.se>
2025-09-30 17:48:31 +0200

    Repetition

--- Övningar/OvningarLecture5Arrayer/ArrayAvInteger.java
+++ Övningar/OvningarLecture5Arrayer/ArrayAvInteger.java
@@ -2,18 +2,24 @@ package OvningarLecture5Arrayer;
 
 import java.util.Arrays;
 import java.util.Random;
-
 public class ArrayAvInteger {
     int [] integerArray;
 
     public ArrayAvInteger(int i) {
         integerArray = new int[i];
-        Random random = new Random();
+        Random r = new Random();
 
-        for (int element = 0; element < integerArray.length; element++) {
-            integerArray[element] = random.nextInt(1000);
+        for(int element = 0; element < integerArray.length; element++) {
+            integerArray[element] = r.nextInt(1000);
         }
     }
+    void substitute(int index, int newValue) {
+        integerArray[index] = newValue;
+    }
+    void square(int index) {
+        integerArray[index] = integerArray[index] * integerArray[index];
+    }
+
     public static void main(String[] args) {
         ArrayAvInteger myArray = new ArrayAvInteger(10);
 
@@ -26,12 +32,5 @@ public class ArrayAvInteger {
         myArray.square(3);
 
         System.out.println(Arrays.toString(myArray.integerArray));
-    }
-    void substitute (int index, int newValue) {
-        integerArray[index] = newValue;
-    }
-
-    void square(int index) {
-        integerArray[index] = integerArray[index] * integerArray[index];
     }
 }
