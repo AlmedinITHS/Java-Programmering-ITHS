0f6f01dc3c125e222433edcceafc5135da961dea
AlmedinITHS <Almedin.Beharovic@ITHS.se>
2025-09-30 14:26:17 +0200

    Repeterat övningar

--- NewTest.java
+++ NewTest.java
@@ -1,13 +1,55 @@
+
 public class NewTest {
+    String make;
+    String model;
+    int year;
+    String colour;
+
+    public NewTest(String make, String model, int year, String colour) {
+        this.make = make;
+        this.model = model;
+        this.year = year;
+        this.colour = colour;
+    }
+    public NewTest(String make, String model, int year) {
+        this(make, model, year,"Unknown");
+    }
+    public NewTest(String make) {
+        this(make, "Unknown", 0, "Unknown");
+    }
+public void printDetails() {
+    System.out.println("Make: " + make);
+    System.out.println("Model: " + model);
+    System.out.println("Year: " + year);
+    System.out.println("Colour: " + colour);
+}
+public static void newLine() {
+    System.out.println("\n");
+}
+
+
+
+    
+            
+    
     public static void main(String[] args) {
-        int [] myArray = {3,5,10, 20,43,17};
+        NewTest car1 = new NewTest("BMW", "Z4", 2002, "silver");
+        car1.printDetails();
 
-        int element = 0;
+        newLine();
 
-        while (element < myArray.length) {
-            System.out.println(myArray[element]);
-            element++;
-        }
+        NewTest car2 = new NewTest("Mercedes", "SLK", 1998, "Blue");
+        car2.printDetails();
+
+        newLine();
+
+        NewTest car3 = new NewTest("Mercedes","Unknown", 0,"Silver");
+        car3.printDetails();
+        
+        
+        
+        
+        
     }
 }
     
