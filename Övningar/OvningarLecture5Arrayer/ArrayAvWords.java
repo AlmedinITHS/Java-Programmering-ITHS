2dbac3e4ccc65a38811a6a392f32246ea41c9bd3
AlmedinITHS <Almedin.Beharovic@ITHS.se>
2025-10-01 09:11:05 +0200

    Kopierad kod

--- Övningar/OvningarLecture5Arrayer/ArrayAvWords.java
+++ Övningar/OvningarLecture5Arrayer/ArrayAvWords.java
@@ -1,34 +1,45 @@
 package OvningarLecture5Arrayer;
 
+import java.util.Arrays;
+
 public class ArrayAvWords {
     public static void main(String[] args) {
-        String [] myArray = {"earth", "wind", "fire", "water"};
+        String [] myArray = {"earth","wind","fire","water","spirit", "darkness", "wouga-bouga"};
 
         int longestWord = 0;
-        int longestWordIndex = 0;
+        int longesWordIndex = 0;
 
         for (int index = 0; index < myArray.length; index++) {
             if (myArray[index].length() > longestWord) {
-                longestWordIndex = index;
+                longestWord = index;
             }
         }
 
-        System.out.println("The longest word in the array is: " + myArray[longestWordIndex]);
+        System.out.println("The longest word in the array is: " + myArray[longesWordIndex]);
 
-        int [] vowelCountArray = new int[myArray.length];
+        int [] vowelCountArray = new int [myArray.length];
         int index = 0;
 
-        for (String word : myArray) {
+        for(String word : myArray) {
             word.toLowerCase();
             int vowelCount = 0;
-            for (int i = 0;i < word.length(); i++) {
+            for(int i = 0; i < word.length();i++) {
                 if (isVowel(word.charAt(i)) == true) {
                     vowelCount++;
                 }
+
             }
             vowelCountArray[index] = vowelCount;
             index++;
         }
+
+        System.out.println(Arrays.toString(vowelCountArray));
     }
-    
+    static boolean isVowel(char c) {
+        if (c == 'a' || c == 'e' || c == 'i' ||c == 'o' || c == 'u' || c == 'y') {
+            return true;
+        }
+        else 
+        return false;
+    }
 }
