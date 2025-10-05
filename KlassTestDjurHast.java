b422daaba3b1b18cdaea37715306e366ab9b4ad1
AlmedinITHS <Almedin.Beharovic@ITHS.se>
2025-10-05 14:06:31 +0200

    Bytt namn och gjort färdigt enkla metoder och seperat klass

---
+++ KlassTestDjurHast.java
@@ -0,0 +1,23 @@
+public class KlassTestDjurHast {
+    int age = 10;
+    double weightInKg = 550;
+    String name = "Penny";
+    void gnagga() {
+        System.out.println("GNÄÄÄGGÄGÄGÄGÄGA");
+    }
+    void stegra() {
+        System.out.println("Hästen stegrar!");
+    }
+    KlassTestDjurHast(int age, double weightInKg, String name) {
+        this.age = age;
+        this.weightInKg = weightInKg;
+        this.name = name;
+    }
+    KlassTestDjurHast(int age) {
+        this(5,350, "Ferrari");
+    }
+    KlassTestDjurHast() {
+        this(1,80,"Newborn horse");
+    }
+
+}
--- MainTestdjur.java
+++ MainTestdjur.java
@@ -1,15 +1,15 @@
 
-public class NewTest {
-    int age;
+public class MainTestdjur {
+     int age;
     double weightInKg;
     String name;
     public static void main(String[] args) {
-        NewTest dog1 = new NewTest();
-        NewTestNyttDjur hast1 = new NewTestNyttDjur();
+        MainTestdjur dog1 = new MainTestdjur();
+        KlassTestDjurHast hast1 = new KlassTestDjurHast();
 
         dog1.name = "Doris";
-        dog1.age = 1;
-        dog1.weightInKg = 1.6;
+        dog1.age = 2;
+        dog1.weightInKg = 100;
 
         System.out.println("Namn: " + dog1.name + " Ålder: " + dog1.age + " Vikt: " + dog1.weightInKg);
 
@@ -18,6 +18,11 @@ public class NewTest {
         
 
         System.out.println(hast1.age +"\n"+ hast1.name +"\n"+ hast1.weightInKg);
+
+        KlassTestDjurHast hast2 = new KlassTestDjurHast(3);
+
+        System.out.println(hast2);
+        
 
 
         
