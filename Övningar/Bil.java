fb3b2f1070ce56c9de8281a4c0032d52c6f0f428
AlmedinITHS <Almedin.Beharovic@ITHS.se>
2025-10-06 15:02:37 +0200

    Saved

--- Övningar/Bil.java
+++ Övningar/Bil.java
@@ -3,12 +3,18 @@ import java.util.Scanner;
 import java.util.Random;
 
 public class Bil extends Fordon {
-    price = 200000;
     
-   public void bil(int price, int weightInKg) {
+   public  Bil(int price, int weightInKg) {
        price = 200000;
        weightInKg = 900;
    
+   }
+   public void speedometer() {
+    for(int i = 0; i < 200; i++) {
+        System.out.println(i);
+
+
+    }
    }
     
     
