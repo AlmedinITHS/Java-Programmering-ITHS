106a674d0d82d65a78a30cc3d15f852faed3e46c
AlmedinITHS <Almedin.Beharovic@ITHS.se>
2025-10-10 12:01:07 +0200

    Ägare

---
+++ PolymorfismEgenOvning/PolymorfismUppgift/Uppgift1/Ager.java
@@ -0,0 +1,10 @@
+package PolymorfismEgenOvning.PolymorfismUppgift.Uppgift1;
+
+ public interface Ager {
+    public String namn;
+    public int age;
+    public Agare(String namn, int age) {
+      
+    }
+
+ }
