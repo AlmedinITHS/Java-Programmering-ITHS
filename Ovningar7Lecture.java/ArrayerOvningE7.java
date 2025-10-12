2edaa45f019cbf26354a08d77083ff5abe136ce6
AlmedinITHS <Almedin.Beharovic@ITHS.se>
2025-10-12 15:11:00 +0200

    aSD

--- Ovningar7Lecture.java/ArrayerOvningE7.java
+++ Ovningar7Lecture.java/ArrayerOvningE7.java
@@ -1,5 +1,14 @@
-package Ovningar7Lecture.java;
+package Ovningar7Lecture;
+
 
 public class ArrayerOvningE7 {
+    public static void main(String[] args) {
+        int [] myArray = new int[4];
+
+        System.out.println(myArray.length);
+
+        
+
+    }
     
 }
