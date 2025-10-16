70cbfc581f9d5f2eb7e030e695f76da5a6eb42f9
AlmedinITHS <Almedin.Beharovic@ITHS.se>
2025-10-16 14:13:19 +0200

    Repeat exercise done, start at 0

--- Övningar/OvningarLecture3Classes/book.java
+++ Övningar/OvningarLecture3Classes/book.java
@@ -1,29 +1,4 @@
-package OvningarLecture3Classes;
 
-public class Book {
-    public String title;
-    public String author;
-    public int year;
 
-    
-        public Book() {
-            title = "La carte et le territoire";
-            author = "Michel Houellbec";
-            year = 2011;
-        }
-        public void printDetails() {
-            System.out.println("Title: " + title);
-            System.out.println("Author: " + author);
-            System.out.println("Year:  " + year);
-        }
-        public static void main(String[] args) {
-            
-            Book book1 = new Book();
-
-            book1.printDetails();
-        
-        
-    }
-}
 
 
--- Övningar/OvningarLecture3Classes/student.java
+++ Övningar/OvningarLecture3Classes/student.java
@@ -1,26 +1,2 @@
-package OvningarLecture3Classes;
 
-public class Student {
-    public String name;
-    public int age;
-    public double grade;
- 
-public Student(int age, double grade, String name) {
-   this.name = name;
-   this.age = age;
-   this.grade = grade;  
-   }
-   public void printDetails() {
-    System.out.println("Name: " + name);
-    System.out.println("Age: " + age);
-    System.out.println("Grade: " + grade);
-   }
-
-   public static void main(String[] args) {
-   Student student1 = new Student(30, 7.6, "Dino");
-
-    student1.printDetails();
-    
-   }
-}
 
