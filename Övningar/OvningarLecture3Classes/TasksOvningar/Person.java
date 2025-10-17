e5024ccfd07aca19342d2f8de1cef9ec7b93bd13
AlmedinITHS <Almedin.Beharovic@ITHS.se>
2025-10-17 10:22:03 +0200

    Ändrat

--- Övningar/OvningarLecture3Classes/TasksOvningar/Person.java
+++ Övningar/OvningarLecture3Classes/TasksOvningar/Person.java
@@ -4,23 +4,19 @@ public class Person {
     public String name;
     public int age;
     public String occupation;
-    public boolean Singlestatus;
+    public boolean active;
 
-    public Person(String name, int age, String occupation, boolean status) {
+    public Person(String name, int age, String occupation, boolean active) {
         this.name = name;
         this.age = age;
         this.occupation = occupation;
-        this.Singlestatus = status;
+        this.active = active;
     }
     public void printDetails() {
         System.out.println("Name: " + name);
         System.out.println("Age: " + age);
         System.out.println("Occupation: " + occupation);
-        System.out.println("Singlestatus: " + Singlestatus + "\n");
+        System.out.println("Active: " + active + "\n");
     }
-    public static void main(String[] args) {
-        Person person1 = new Person("Dino", 36, "it-consultant", true);
-
-        person1.printDetails();
-    }
+    
 }
--- Övningar/OvningarLecture3Classes/TasksOvningar/PersonMain.java
+++ Övningar/OvningarLecture3Classes/TasksOvningar/PersonMain.java
@@ -2,9 +2,9 @@ package OvningarLecture3Classes.TasksOvningar;
 
 public class PersonMain {
     public static void main(String[] args) {
-
         ContainerClass work1 = new ContainerClass("ITier", "Valentine's adelaide 46, New hampshire", "Office");
         ContainerClass work2 = new ContainerClass("FrigoScandia", "Scandinavian street 105, Ohio", "Storage");
+
 
         work2.printWorkDetails();
 
@@ -14,11 +14,18 @@ public class PersonMain {
 
         work2.printWorkDetails();
 
-        Person person1 = new Person("Dino", 36, "Software tester", true);
+    
+
+        Person person1 = new Person("Hans", 36, "Software tester", true);
         Person person2 = new Person("Anna",32,"Software developer",true);
         Person person3 = new Person("Peter",29,"Software tester", false);
         Person person4 = new Person("Viktoria", 25, "cleaning", false);
-        
+        //System.out.println("Person1: " + person1 + "Person2: " + person2 + "Person3: " + person3 + "Person4: " + person4);
+
+        person1.occupation = "Lead developer";
+
+        System.out.println("New occupation: " + person1.occupation);
+
         
     }
     
